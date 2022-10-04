02ba9b7551b9b0ac84182f835cbd43cc9a004663
E-maiL981 <72767491+E-maiL981@users.noreply.github.com>
2022-10-03 21:31:28 -0400

    added timers

---
+++ ece2049a22_lab3_omkrause_ldjennings/timer.c
@@ -0,0 +1,22 @@
+/*
+ * timer.c
+ *
+ *  Created on: Oct 3, 2022
+ *      Author: Liam
+ */
+#include <msp430.h>
+#include "timer.h"
+
+unsigned long int timer_cnt = 0;
+
+void runtimerA2(void)
+{
+  TA2CTL = TASSEL_1 | MC_1 | ID_0;
+  TA2CCR0 = 32767;
+  TA2CCTL0 = CCIE;     // enable Timer A2 interrupt
+}
+
+#pragma vector=TIMER2_A0_VECTOR
+__interrupt void TIMER2_A0_ISR (void) {
+    timer_cnt++;
+}
---
+++ ece2049a22_lab3_omkrause_ldjennings/timer.h
@@ -0,0 +1,18 @@
+/*
+ * timer.h
+ *
+ *  Created on: Oct 3, 2022
+ *      Author: Liam
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+
+extern unsigned long int timer_cnt;
+
+
+void setupTimerA2();
+
+
+#endif /* TIMER_H_ */
